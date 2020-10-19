8004fceb05beeb0dc15254cabdbe58e24deda0ec
tarz2929 <saitarunmulley@gmail.com>
2020-10-19 10:00:05 -0600

    added message

--- CSharpReview_4point2/Program.cs
+++ CSharpReview_4point2/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpReview_4point2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! and hello canada");
         }
     }
 }
